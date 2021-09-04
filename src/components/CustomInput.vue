7dee0e592feb28a9d73a5add8de6a1a8142ba350
lixingyang-lxy <1433565248@qq.com>
2021-09-04 16:16:57 +0800

    parent pass function to child

--- src/components/CustomInput.vue
+++ src/components/CustomInput.vue
@@ -1,27 +1,31 @@
 <template>
   <div>
-    <input type="text" :value="value" @input="test" />
-    <!-- <input type="checkbox" :checked="checked" @change="test" /> -->
+    <!-- <input type="text" :value="value" @input="test" /> -->
+    <input type="checkbox" :checked="checked" @change="test" />
     <!-- <input type="button" value="click" /> -->
   </div>
 </template>
 <script>
 export default {
-  props: ["value"],
-  methods: {
-    test(e) {
-      this.$emit("input", e.target.value);
-    },
-  },
-  // props: ["checked"],
-  // model: {
-  //   props: "checked",
-  //   event: "change",
-  // },
+  // props: ["value"],
   // methods: {
   //   test(e) {
-  //     this.$emit("change", e.target.checked);
-  //   },
+  //     debugger
+  //     this.$emit("input", e.target.value);
+      
+  //     this.$listeners.test('i am child')
+  //   }
   // },
+  props: ["checked"],
+  model: {
+    props: "checked",
+    event: "change",
+  },
+  methods: {
+    test(e) {
+      this.$emit("change", e.target.checked);
+      this.$listeners.test('i am child')
+    },
+  },
 };
 </script>
--- src/test/Test.vue
+++ src/test/Test.vue
@@ -1,24 +1,28 @@
 <template>
   <div>
-    <CustomInput v-model='msg' @click.native="test" />
+    <!-- <CustomInput v-model="msg" @click.native="test" /> -->
+    <CustomInput v-model="msg" @test="parentTest" @click.native="test" />
   </div>
 </template>
 
 <script>
-import CustomInput from '../components/CustomInput.vue'
+import CustomInput from "../components/CustomInput.vue";
 export default {
   data() {
     return {
-      msg: ''
-    }
+      msg: "",
+    };
   },
   methods: {
     test() {
-      console.log('click test')
-    }
+      console.log("click test");
+    },
+    parentTest(content) {
+      console.log(this, "content: " + content);
+    },
   },
   components: {
-    CustomInput
-  }
+    CustomInput,
+  },
 };
 </script>
